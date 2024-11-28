a0816c66d9d12136f07227ef18bf77be98501b8b
AdamStajek <adasta146@gmail.com>
2024-11-28 16:08:27 +0100

    Added base class type hint

---
+++ test/test_valentim.py
@@ -0,0 +1,27 @@
+from pycx import pycxsimulator
+from models.valentim import valentim
+import json
+from models.model import Model
+
+with open('models/valentim/valentim_scenarios.json', 'rb') as fh:
+    scenarios = json.load(fh)
+
+scenario = scenarios["scenario_3"]["case_1"]
+
+M = 128
+model: Model = valentim.ValentimModel(M, scenario)
+
+
+def initialize():
+    model.reset()
+
+
+def observe():
+    model.plot_lattice()
+
+
+def update():
+    model.make_step()
+
+
+pycxsimulator.GUI().start(func=[initialize, observe, update])
