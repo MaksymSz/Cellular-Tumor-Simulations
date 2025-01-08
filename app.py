06a8429076c1b7dac84abd8c7c02bb27e142df01
AdamStajek <adasta146@gmail.com>
2025-01-08 22:06:32 +0100

    plot_simulation func

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from pycx.inter_gui import get_simulation_params
 from pycx import pycxsimulator
 from models.ghaemi.ghaemi_gpu import GhaemiModel
 from models.valentim.valentim import ValentimModel
-from models.sato.sim import initialize, step, draw
+from models.sato.sim import initialize, step, draw, plot_simulation
 
 
 def main():
@@ -16,7 +16,7 @@ def main():
     if model:
         pycxsimulator.GUI().start(func=[model.reset, model.plot_lattice, model.make_step, model.plot_simulation])
     else:
-        pycxsimulator.GUI().start(func=[initialize, draw, step, lambda x: x])
+        pycxsimulator.GUI().start(func=[initialize, draw, step, plot_simulation])
 
 
     
